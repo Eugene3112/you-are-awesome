e099dfde664ff8518fd544b11253f24470c3b4b6
Eugene3112 <bertoshevgieniy@gmail.com>
2018-10-07 19:48:18 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,58 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = () => {{}};
+const createNotEnumerableProperty = (property) => {
+    Object.defineProperty(Object.prototype, property, {
+        get: function () {
+            return property
+        },
+        set: function (value) {
+            property = value;
+        }
+    });
+    return property;
+};
+
+const createProtoMagicObject = () => {
+    let magicObject = function (){};
+    magicObject.prototype = magicObject.__proto__;
+    return magicObject;
+};
+const incrementor = (() => {
+    let additional = 0;
+    let fCall = function makeAdditional() {
+        additional++;
+        return makeAdditional;
+    };
+    fCall.valueOf = () => additional;
+    return fCall;
+})();
+const asyncIncrementor = (() => {
+    let additional = 0;
+    let fCall = function makeAdditional() {
+        additional++;
+        return new Promise(resolve => resolve(makeAdditional));
+    };
+    fCall.valueOf = () => additional;
+    return fCall;
+})();
+const createIncrementer = function* (){
+    let index = 0;
+    while(true)
+        yield ++index;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (x) => new Promise(resolve => {
+    setTimeout(() => resolve(x), 1021)
+});
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {return null};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (array) => {
+    array.sort((a, b) => a.__proto__ -b.__proto__);
+    return array;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
